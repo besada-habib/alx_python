a5395d34ee1c177227bf31565c5f2e3ec7c669c1
besada-habib <besadasamy@gmail.com>
2023-12-15 00:27:44 +0200

    04

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-from math import copysign
+import math
 import random
 number = random.randint(-10000, 10000)
 
-last_digit_of_number = int(repr(number)[-1])
+
+last_digit_of_number = abs(int(repr(number)[-1]))
 if last_digit_of_number > 5:
-    print( "Last digit of",copysign(number, -1), "is", last_digit_of_number ,"is greater than 5")
+    print( "Last digit of",number, "is", last_digit_of_number ,"is greater than 5")
 elif last_digit_of_number == 0:
-    print ("Last digit of",copysign(number,-1), "is", last_digit_of_number , "and is 0")
+    print ("Last digit of",number, "is", last_digit_of_number , "and is 0")
 else:
-    print("Last digit of",copysign(number,-1), "is", last_digit_of_number , "and is less than 6 and not 0")
+    print("Last digit of", number,  "is", last_digit_of_number , "and is less than 6 and not 0")
     
---
+++ python-hello_world/4-print_hexa.py
@@ -0,0 +1,4 @@
+number = 0
+while number <= 98 :
+    print(hex (number))
+    number =number + 1
