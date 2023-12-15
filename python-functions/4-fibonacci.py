bb8b1b5ea46ac7457ddf3179965d823ccfbf8aa1
besada-habib <besadasamy@gmail.com>
2023-12-15 03:08:02 +0200

    15

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,8 +1,10 @@
 def fibonacci_sequence(n):
     sequence = [0, 1]
+    if n <= 0:
+        return [0]
 
     while len(sequence) < n:
         next_term = sequence[-1] + sequence[-2]
         sequence.append(next_term)
 
-    return sequence [:n]
+    return sequence
