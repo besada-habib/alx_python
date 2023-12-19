d8c7b581b34362c3a65f17747a117c7244078cd5
besada-habib <besadasamy@gmail.com>
2023-12-19 23:04:40 +0200

    31

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -1,10 +1,10 @@
-
-if __name__ == "__main__":
- def safe_print_division(a, b):
+def safe_print_division(a, b):
      try:
        result = a / b
      except ZeroDivisionError:
-       pass
+       result = None
      finally:
          print("Inside result: {}".format(result))
          return result
+if __name__ == "__main__":
+    safe_print_division()
