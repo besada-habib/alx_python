e13b3c4819360f39c7604a042b5f115ca702d608
besada-habib <besadasamy@gmail.com>
2023-12-20 00:44:19 +0200

    33

--- python-import_modules/4-raise_exception.py
+++ python-import_modules/4-raise_exception.py
@@ -1,5 +1,5 @@
-raise_exception = __import__('4-raise_exception').raise_exception
-
+def raise_exception():
+    raise_exception = __import__('4-raise_exception').raise_exception
 try:
     raise_exception(1)
 except TypeError as te:
