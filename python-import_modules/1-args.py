616f7f1f691f8c76c75e7f32f0a8fbc113458a3a
besada-habib <besadasamy@gmail.com>
2023-12-19 22:34:53 +0200

    25

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -5,7 +5,6 @@ if __name__ == "__main__":
      arg_n = len(sys.argv) - 1 
      if arg_n == 0:
       print("0 arguments.")
-      print(":")
      elif arg_n == 1:
         print("1 argument:")
      else:
