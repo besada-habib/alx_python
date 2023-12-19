d12c80ee6a98e69d24d3cd1020d3c3b51e29afa5
besada-habib <besadasamy@gmail.com>
2023-12-19 22:30:49 +0200

    22

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -2,17 +2,17 @@ import sys
 
 if __name__ == "__main__":
     def print_arguments ():
-     num_of_args = len(sys.argv) - 1 
+     arg_n = len(sys.argv) - 1 
 
-if num_of_args == 0:
-       print("0 argument.")
-       print(":")
-    elif num_of_args == 1
+if arg_n == 0:
+    print("0 argument.")
+    print(":")
+elif arg_n == 1:
         print("1 argument:")
-     else:
-        print("{} arguments:".format(num_of_args))
+else:
+        print("{} arguments:".format(arg_n))
 
-    for i, arg in enumerate(sys.argv[1:], start=1):
+for i, arg in enumerate(sys.argv[1:], start=1):
         print("{}: {}".format(i, arg))
      
-    print_arguments()
+print_arguments()
