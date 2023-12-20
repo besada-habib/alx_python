cc7d8df277657089921079bd54546d2908f587d8
besada-habib <besadasamy@gmail.com>
2023-12-20 03:11:06 +0200

    01

--- python-data_structures/2-multiple_returns.py
+++ python-data_structures/2-multiple_returns.py
@@ -1,16 +1,10 @@
 def multiple_returns(sentence):
-    # Check if the sentence is empty
     if not sentence:
         return (0, None)
     
-    # Return a tuple with the length and the first character of the sentence
     return (len(sentence), sentence[0])
 
-# Example usage:
-sentence1 = "Hello, World!"
-result1 = multiple_returns(sentence1)
-print(result1)  # Output: (13, 'H')
-
-sentence2 = ""
-result2 = multiple_returns(sentence2)
-print(result2)  # Output: (0, None)
+if __name__ == "__main__":
+    sentence = "At Holberton school, I learnt C!"
+    length, first = multiple_returns(sentence)
+    print("Length: {:d} - First character: {}".format(length, first))
